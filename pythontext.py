14e3576e5f76476e3db7d78552b4aa6487eb881e
milleriishaun <milleriishaun@gmail.com>
2018-02-21 14:25:43 -0800

    Debugging VSC Debugger

---
+++ pythontext.py
@@ -0,0 +1,15 @@
+import random
+import sys
+import os
+
+
+print("Hello World")
+
+name = "Shaun"
+print(name)
+
+# Numbers, Strings, Lists, Tuples, Dictionaries
+
+name = 15
+print(name)
+print("5 // 2=", 5//2)
