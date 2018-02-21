de816bdf9b04245c660dbde2bae9f45c78c5d5cb
milleriishaun <milleriishaun@gmail.com>
2018-02-21 15:55:28 -0800

    Test Manipulation of Lists

--- pythontext.py
+++ pythontext.py
@@ -2,14 +2,52 @@ import random
 import sys
 import os
 
+grocery4 = ['Juice', "tomato", 'grapes']
 
-print("Hello World")
+print('First Item: ', grocery4[0])
 
-name = "Shaun"
-print(name)
+print(grocery4)
 
-# Numbers, Strings, Lists, Tuples, Dictionaries
+grocery4[0] = "Green Juice"
 
-name = 15
-print(name)
-print("5 // 2=", 5//2)
+print('Actual First Item: ', grocery4[0])
+
+print(grocery4)
+grocery4.insert(0, 'Toothpaste')
+print(grocery4[0:5])
+print(grocery4[0:4])
+
+grocery4.append('Soap')
+
+print(grocery4[0:5])
+
+print(grocery4.extend(['more', 'and more']))
+
+print(grocery4)
+
+print(grocery4[0:7])
+
+print('Number index of more: ', grocery4.index('more'))
+
+other_events = ['Wash Car', 'Pick up', 'ash check']
+to_do = [other_events, grocery4]
+print(to_do)
+print(to_do[1][0])
+print('\n' *3, grocery4)
+grocery4.append('Onions')
+print(grocery4)
+
+print(to_do)
+
+grocery4.insert(1, 'Pickle')
+
+# sorted() creates a new list
+# sort() changes the old list, mutating it, and you cannot go back
+
+print('\n'*2)
+print("1 ", grocery4)
+print("2 ", grocery4)
+print("3 ", sorted(grocery4))
+print("4 ", grocery4)
+grocery4.sort() # Not printed
+print("5 ", grocery4)
