2a52526a5fe7ebf38cf5b9284df22aec3c1adb1d
milleriishaun <milleriishaun@gmail.com>
2018-02-24 21:34:01 -0800

    Added Unicode conversion

--- stringmanipulation.py
+++ stringmanipulation.py
@@ -14,4 +14,7 @@ for char in samp_string:
 for i in range(0, len(samp_string)-1,2):
     print(samp_string[i] + samp_string[i+1])
 
-
+# A-Z have unicodes 65-90
+# a-z have unicodes from 97-122
+print("A =", ord("A"))
+print("65 =", chr(65))
