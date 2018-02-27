f20a67f22c57ef1009a9d8127f8afacab5a97a57
milleriishaun <milleriishaun@gmail.com>
2018-02-26 19:28:34 -0800

    Edited Solve_x

--- Solve_x funct.py
+++ Solve_x funct.py
@@ -26,3 +26,12 @@ def solve_eq(equation):
     return "x = " + str(num2 - num1)
 
 print(solve_eq("x + 4 = 9"))
+
+
+# Extra code example of multioperation funct
+def mult_divide(num1, num2):
+    return (num1 * num2), (num1 / num2)
+
+mult, divide = mult_divide(5, 4)
+print("5 * 4 = ", mult)
+print("5 / 4 = ", divide)
