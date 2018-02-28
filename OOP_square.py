82fea13dcd517ffe9679a4c44f32ac8be22a770f
milleriishaun <milleriishaun@gmail.com>
2018-02-28 11:13:54 -0800

    Added OOP_square

---
+++ OOP_square.py
@@ -0,0 +1,58 @@
+class Square:
+
+    def __init__(self, height="0", width="0"):
+        self.height = height
+        self.width = width
+
+    # Getter
+    # allows reference to individual fields
+    @property
+    def height(self):
+        print("Retrieving the Height")
+
+        # use __ to show that information is private
+        return self.__height
+
+    # Setter
+    # protect form putting in bad data
+    @height.setter
+    def height(self, value):
+
+        if value.isdigit():
+            self.__height = value
+        else:
+            print("Please only enter numbers for height")
+
+    @property
+    def width(self):
+        print("Retrieving the Width")
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+
+        if value.isdigit():
+            self.__width = value
+        else:
+            print("Please only enter numbers for width")
+
+    def getArea(self):
+        return int(self.__width) * (int(self.__height))
+
+def main():
+    aSquare = Square()
+
+    height = input("Enter Height: ")
+    width = input("Enter Width: ")
+
+    # because we use @property and @height.setter, don't need height()
+    aSquare.height = height
+    aSquare.width = width
+
+    print("Height: ", aSquare.height)
+    print("Width: ", aSquare.width)
+
+    print("The area is: ", aSquare.getArea())
+
+main()
