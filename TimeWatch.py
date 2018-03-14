c14d3e111ca0a6596aea0021fa53abcba529d9c7
milleriishaun <milleriishaun@gmail.com>
2018-03-14 10:53:39 -0700

    TimeWatch_GUI Initial Commit

---
+++ TimeWatch.py
@@ -0,0 +1,17 @@
+# Make this GUI for SOMETHING!
+# need GUI for TimeWatch app!
+
+# Make the clock system
+
+
+# import the modules
+import sys
+import os
+import math
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+
+
+
+# get the current time
