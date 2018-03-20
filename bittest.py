eff785e6bfe399664beade4055331e4fea8c3cba
milleriishaun <milleriishaun@gmail.com>
2018-03-20 14:08:18 -0700

    Edited bittest

--- bittest.py
+++ bittest.py
@@ -1,9 +1,11 @@
 # use requests to get data in json form
 import requests
 
+
+
 r = requests.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2016-03-19&end=2018-03-19', verify=False)
 
-# real data from past month
+# real data from few years
 # get the BitCoin Price Index
 for k, v in r.json()['bpi'].items():
     print(k, v)
