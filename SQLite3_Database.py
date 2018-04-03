bff9895546c63f28047dd23dea0f1ca0184ed0d6
milleriishaun <milleriishaun@gmail.com>
2018-04-03 11:31:01 -0700

    Add SQL database implementation

--- SQLite3_Database.py
+++ SQLite3_Database.py
@@ -192,8 +192,147 @@ print("Database Closed")
 # complicated queries might make it fail, but there are a lot of extensions
 # they can be used in any language
 
+
+# Try number 2
 import sqlite3
 import sys
 
-# more edits of the database sqlite3
-print("Database manipulation within sqlite3")
+def printDB():
+    
+    try:
+        result = theCursor.execute("SELECT ID, FName, LName, Age, Address, Salary, HireDate FROM Employees2")
+        
+        for row in result:
+            print("ID :", row[0])
+            print("FName :", row[1])
+            print("LName :", row[2])
+            print("Age :", row[3])
+            print("Address :", row[4])
+            print("Salary :", row[5])
+            print("HireDate :", row[6])
+        
+    except sqlite3.OperationalError:
+        print("The Table doesn't exist")
+    
+    except:
+        print("Couldn't retrieve data from database")
+
+db_conn = sqlite3.connect('test.db')
+
+print("Database Created")
+
+# Cursor allows traversing through what we wantto do in DB
+theCursor = db_conn.cursor()
+
+db_conn.execute("DROP TABLE IF EXISTS Employees2")
+db_conn.commit()
+
+try:
+
+    db_conn.execute("CREATE TABLE Employees2(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, FName TEXT NOT NULL, LName TEXT NOT NULL, Age INTEGER NOT NULL, Address TEXT, Salary REAL, HireDate TEXT);")
+
+    db_conn.commit()
+
+except sqlite3.OpreationalError:
+    print("Table Couldn't be Created")
+
+print("Table Created")
+
+# insert data into our DB
+db_conn.execute("INSERT INTO Employees2(FName, LName, Age, Address, Salary, HireDate) VALUES('Shaun', 'Miller', 41, '123 Main Street', 500000, date('now'))")
+db_conn.commit()
+
+# this is just a printing function to get the info in the DB printed
+printDB()
+
+# Updating the information is something we would want to do all the time
+# this is common for manipulating databases
+try:
+    db_conn.execute("UPDATE Employees2 SET Address = '121 Main St' WHERE ID=1")
+    db_conn.commit()
+
+except sqlite3.OperationalError:
+    print("Table Couldn't Be Updated")
+
+printDB()
+
+'''
+# Let's also learn how to delete individual tables and parts
+try:
+    db_conn.execute("DELETE FROM Employees2 WHERE ID=1")
+    db_conn.commit()
+
+except sqlite3.OperationalError:
+    print("Table couldn't be deleted")
+
+printDB()
+
+db_conn.rollback()
+'''
+
+# can add new columns and pieces of data to the tables
+try:
+    db_conn.execute("ALTER TABLE Employees2 ADD COLUMN 'Image' BLOB DEFAULT NULL")
+    db_conn.commit()
+
+except sqlite3.OperationalError:
+    print("Table couldn't be updated")
+
+# we can also retrieve our DB table names
+theCursor.execute("PRAGMA TABLE_INFO(Employees2)")
+
+# fetch all the different results from where the cursor is, after pragma
+# we use a list comprehension to get the second index 
+# fetchall returns all teh rows of data that ww're looking for
+# explaination: 1. theCursor is a variable that holds all the rowdata
+# 2. nameTuple is just a variable denoting the index position in the (theCursor list/variable)
+# 3. For each of the index positions, make them equivalent to the second index
+# 4. Store the new list of 
+
+# just some testing
+t = theCursor.fetchall()
+print(t)
+
+
+rowNames = [nameTuple[1] for nameTuple in theCursor.fetchall()]
+print(rowNames)
+
+# the below works correctly but not through the above list comprehension
+r = []
+for x in t:
+    r.append(x[1])
+print(r)
+
+# random extra thing #1,
+# get the total number of rows
+theCursor.execute("SELECT COUNT(*) FROM Employees2")
+numOfRows = theCursor.fetchall()
+print("Total Rows: ", numOfRows[0][0])
+
+# random extra thing #2,
+# get the version of the sqlite we are using
+theCursor.execute("SELECT SQLITE_VERSION()")
+print("SQLite Version: ", theCursor.fetchone()) # use fetchone bc 1 result
+
+# random exta thing #3,
+# use a dictionary cursor to retrieve data within a dictionary
+with db_conn:
+    db_conn.row_factory = sqlite3.Row
+    # this is a call for a new cursor
+    theCursor = db_conn.cursor()
+    theCursor.execute("SELECT * FROM Employees2")
+    # new variable called rows
+    rows = theCursor.fetchall()
+
+    # now cycle through the results
+    for row in rows:
+        print("{} {}".format(row['FName'], row['LName']))
+
+
+# let's write data to a file... like for backups of the DB data
+with open('duump.sql', 'w') as f:
+    for line in db_conn.iterdump(): # iterdump returns an iterator into SQL format
+        f.write("%s\n" % line)
+db_conn.close()
+
+print("Database Closed")
