48390a9160cfce466d21e931cda722bf6d29f659
milleriishaun <milleriishaun@gmail.com>
2018-04-10 15:32:49 -0700

    Add CustomWidget

--- kivytutorials.py
+++ kivytutorials.py
@@ -17,15 +17,18 @@ helloKivy.run()
 '''
 
 import kivy
-kivy.require("1..9.0")
+kivy.require("1.9.0")
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+
+class CustomWidget(Widget):
+    pass
 
 class CustomWidgetApp(App):
 
     def build(self):
         return CustomWidget()
 
-customWidget = CustomWIdgetApp()
+customWidget = CustomWidgetApp()
 customWidget.run()
