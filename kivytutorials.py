3bf2ef7166b998e82801c6927607d4b08e1b4725
milleriishaun <milleriishaun@gmail.com>
2018-04-10 15:18:54 -0700

    Add HelloKivy

---
+++ kivytutorials.py
@@ -0,0 +1,31 @@
+'''
+
+import kivy
+kivy.require('1.9.0')
+
+from kivy.app import App
+from kivy.uix.button import Label
+
+class HelloKivyApp(App):
+
+    def build(self):
+        return Label()
+
+helloKivy = HelloKivyApp()
+
+helloKivy.run()
+'''
+
+import kivy
+kivy.require("1..9.0")
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class CustomWidgetApp(App):
+
+    def build(self):
+        return CustomWidget()
+
+customWidget = CustomWIdgetApp()
+customWidget.run()
