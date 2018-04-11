d75e5ea45579291657f29f0bc88a899451511591
milleriishaun <milleriishaun@gmail.com>
2018-04-10 17:43:58 -0700

    Add StudentDB Kivy Form

--- kivytutorials.py
+++ kivytutorials.py
@@ -130,6 +130,8 @@ plApp = PageLayoutApp()
 plApp.run()
 '''
 
+
+'''
 import kivy
 kivy.require("1.9.0")
 
@@ -152,3 +154,36 @@ class CalculatorApp(App):
 
 calcApp = CalculatorApp()
 calcApp.run()
+'''
+
+'''
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+from kivy.uix.listview import ListItemButton
+
+class StudentListButton(ListItemButton):
+    pass
+
+class StudentDB(BoxLayout):
+    first_name_text_input = ObjectProperty()
+    last_name_text_input = ObjectProperty()
+    student_list = ObjectProperty()
+
+    def submit_students(self):
+        pass
+
+    def delete_student(self):
+        pass
+    
+    def replace_student(self):
+        pass
+    
+class StudentDBApp(App):
+    def build(self):
+        return StudentDB()
+    
+dbApp = StudentDBApp()
+dbApp.run()
+'''
+
